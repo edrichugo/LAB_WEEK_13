6859519616696067bd431029fdbbe0204d7d5641
edrichugo <hugoedric@gmail.com>
2025-12-04 08:49:58 +0700

    Commit No. 2 – update to Room

--- app/src/main/java/com/example/lab_week_13/MovieRepository.kt
+++ app/src/main/java/com/example/lab_week_13/MovieRepository.kt
@@ -1,18 +1,37 @@
 package com.example.lab_week_13
 
 import com.example.lab_week_13.api.MovieService
+import com.example.lab_week_13.database.MovieDao
+import com.example.lab_week_13.database.MovieDatabase
 import com.example.lab_week_13.model.Movie
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 
-class MovieRepository(private val movieService: MovieService) {
+class MovieRepository(
+    private val movieService: MovieService,
+    private val movieDatabase: MovieDatabase ) {
     private val apiKey = "d7419d594994f6ba171f2d9053c3430e"
 
     fun fetchMovies(): Flow<List<Movie>> {
         return flow {
-            emit(movieService.getPopularMovies(apiKey).results)
+            // Check if there are movies saved in the database
+            val movieDao: MovieDao = movieDatabase.movieDao()
+            val savedMovies = movieDao.getMovies()
+            // If there are no movies saved in the database,
+            // fetch the list of popular movies from the API
+            if(savedMovies.isEmpty()) {
+                val movies = movieService.getPopularMovies(apiKey).results
+                // save the list of popular movies to the database
+                movieDao.addMovies(movies)
+                // emit the list of popular movies from the API
+                emit(movies)
+            } else {
+                // If there are movies saved in the database,
+                // emit the list of saved movies from the database
+                emit(savedMovies)
+            }
         }.flowOn(Dispatchers.IO)
     }
 }
